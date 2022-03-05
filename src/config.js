b4cba92a45f3cf45b7e0b04bd24e6102e7ae4e80
Tyler Siravo <tylersiravo@gmail.com>
2022-03-05 13:58:06 -0500

    rpc var moved

--- src/config.js
+++ src/config.js
@@ -1,7 +1,10 @@
 const ethers = require('ethers')
 
+const RPC = "YOUR_CUSTOM_RPCNODE_URL";
+
 //erc-721 compliant token contract address
 const address = "";
+
 //set how many you would like to have locally
 const collectionSize = 10;
 
@@ -12,7 +15,7 @@ const abi = [
   "function tokenURI(uint256 tokenId) external view returns (string)"
 ];
 
-const provider = new ethers.providers.JsonRpcProvider("YOUR_CUSTOM_RPCNODE_URL");
+const provider = new ethers.providers.JsonRpcProvider(RPC);
 
 const instance = new ethers.Contract(address, abi, provider);
 
